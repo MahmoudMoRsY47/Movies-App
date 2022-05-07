70d02c866f44c97efee2f97166ea8fd3ba84f6a5
Mahmoud Morsy <74903350+MahmoudMoRsY47@users.noreply.github.com>
2022-05-07 14:24:24 +0200

    Update Result.kt

--- app/src/main/java/com/example/moviesapp/model/Result.kt
+++ app/src/main/java/com/example/moviesapp/model/Result.kt
@@ -5,56 +5,58 @@ import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 import java.io.Serializable
+
 @Parcelize
 data class Result(
     @SerializedName("adult")
-    val adult: Boolean?,
+    val adult: Boolean? = false,
     @SerializedName("backdrop_path")
-    val backdropPath: String?,
+    val backdropPath: String? = "",
 
     @SerializedName("budget")
-    val budget: Int?,
+    val budget: Int? = 0,
 
     val key: String = "result",
     @SerializedName("homepage")
-    val homepage: String?,
+    val homepage: String? = "",
     @SerializedName("id")
-    val id: Int?,
+    val id: Int? = 0,
     @SerializedName("imdb_id")
-    val imdbId: String?,
+    val imdbId: String? = "",
     @SerializedName("original_language")
-    val originalLanguage: String?,
+    val originalLanguage: String? = "",
     @SerializedName("original_title")
-    val originalTitle: String?,
+    val originalTitle: String?= "",
     @SerializedName("overview")
-    val overview: String?,
+    val overview: String?= "",
     @SerializedName("popularity")
-    val popularity: Double?,
+    val popularity: Double? = 0.0,
     @SerializedName("poster_path")
-    val posterPath: String?,
+    val posterPath: String? = "",
 
     @SerializedName("release_date")
-    val releaseDate: String?,
+    val releaseDate: String? = "",
     @SerializedName("revenue")
-    val revenue: Int?,
+    val revenue: Int? = 0,
     @SerializedName("runtime")
-    val runtime: Int?,
+    val runtime: Int? = 0,
 
     @SerializedName("status")
-    val status: String?,
+    val status: String? = "",
     @SerializedName("tagline")
-    val tagline: String?,
+    val tagline: String?= "",
     @SerializedName("title")
-    val title: String?,
+    val title: String?= "",
     @SerializedName("video")
-    val video: Boolean?,
+    val video: Boolean? = false,
     @SerializedName("vote_average")
-    val voteAverage: Double?,
+    val voteAverage: Double? = 0.0,
     @SerializedName("vote_count")
-    val voteCount: Int?,
+    val voteCount: Int? = 0,
 
-    @field:SerializedName("origin_country")
-     val originCountry: String? = null
+    @SerializedName("origin_country")
+    val originCountry: String? = ""
 
 
-):Parcelable
+) : Parcelable
+
